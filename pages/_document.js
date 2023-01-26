e764aa64e04ae9af1cc56cf3afb7529a6f8ca2fd
bil4l755 <abdulbilal755755@gmail.com>
2023-01-26 18:24:42 +0530

    blog content done

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-       <Head>
+      <Head>
         <title>Ekonopro</title>
         <meta charset="utf-8" />
         <meta name="description" content="Economics handles the world behind the scene, from a coin to world bank" />
@@ -32,8 +32,19 @@ export default function Document() {
       </Head>
       <body>
         <Main />
+        <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
         <NextScript />
-        
+        {/* <!-- Vendor JS Files --> */}
+        < script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js" ></script >
+        <script src="assets/vendor/aos/aos.js"></script>
+        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
+
+        {/* <!-- Template Main JS File --> */}
+        <script src="assets/js/main.js"></script>
       </body>
     </Html>
   )
