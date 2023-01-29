07bef8b7540fdd3e5a91612bf894f21a06e2358b
bil4l755 <abdulbilal755755@gmail.com>
2023-01-29 14:12:22 +0530

    Blog system done

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,8 @@ export default function Document() {
         <meta content="economics" name="keywords" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <!-- Favicons --> */}
-        <link href="assets/img/favicon.png" rel="icon" />
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/assets/img/favicon.png" rel="icon" />
+        <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
         <link rel="icon" href="/favicon.ico" />
 
         {/* <!-- Google Fonts --> */}
@@ -20,14 +20,14 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
         {/* <!-- Vendor CSS Files --> */}
-        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-        <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
-        <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
-        <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
-        <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+        <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+        <link href="/assets/vendor/aos/aos.css" rel="stylesheet" />
+        <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+        <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
 
         {/* <!-- Template Main CSS File --> */}
-        <link href="assets/css/main.css" rel="stylesheet" />
+        <link href="/assets/css/main.css" rel="stylesheet" />
 
       </Head>
       <body>
@@ -35,16 +35,16 @@ export default function Document() {
         <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
         <NextScript />
         {/* <!-- Vendor JS Files --> */}
-        < script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js" ></script >
-        <script src="assets/vendor/aos/aos.js"></script>
-        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-        <script src="assets/vendor/php-email-form/validate.js"></script>
+        < script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" ></script >
+        <script src="/assets/vendor/aos/aos.js"></script>
+        <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="/assets/vendor/php-email-form/validate.js"></script>
 
         {/* <!-- Template Main JS File --> */}
-        <script src="assets/js/main.js"></script>
+        <script src="/assets/js/main.js"></script>
       </body>
     </Html>
   )
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,238 @@
 import client from '../../client'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
 
-const Post = ({post}) => {
-  
+const Post = ({ post }) => {
+  console.log(post)
   return (
-    <article>
-      <h1>{post?.slug?.current}</h1>
-    </article>
+    // <article>
+    //   <h1>{post?.slug?.current}</h1>
+    // </article>
+    <>
+      <Header />
+      <main id="main">
+        {/* <!-- ======= Breadcrumbs ======= --> */}
+        <div className="breadcrumbs">
+          <div className="page-header d-flex align-items-center" >
+            <div className="container position-relative">
+              <div className="row d-flex justify-content-center">
+                <div className="col-lg-6 text-center">
+                  <h2>Blog Details</h2>
+                  <p>These blogs are to increase your economic knowledge of current affair and basics of economics.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <nav>
+            <div className="container">
+              <ol>
+                <li><a href="/">Home</a></li>
+                <li>Blog Details</li>
+              </ol>
+            </div>
+          </nav>
+        </div>
+        {/* <!-- End Breadcrumbs --> */}
+
+
+        {/* <!-- ======= Blog Details Section ======= --> */}
+        <section id="blog" className="blog bg-light">
+          <div className="container" data-aos="fade-up">
+
+            <div className="row g-5">
+
+              <div className="col-lg-8">
+
+                <article className="blog-details">
+
+                  <div className="post-img">
+                    <img src="/assets/img/blog/blog-1.jpg" alt="" className="img-fluid" />
+                  </div>
+
+                {/* post title  */}
+                  <h2 className="title">{post?.title}</h2>
+
+                  <div className="meta-top">
+                    <ul >
+                      <li className="d-flex align-items-center"><i className="bi bi-person"></i> Mr. XYZ</li>
+                      <li className="d-flex align-items-center"><i className="bi bi-clock"></i> <time dateTime="2020-01-01">{post?.publishedAt}</time></li>
+                      <li className="d-flex align-items-center"><i className="bi bi-chat-dots"></i> 12 Comments</li>
+                    </ul>
+                  </div>
+                  {/* <!-- End meta top --> */}
+
+                  <div className="content text-black">
+                    <p>
+                      {post?.body[0]?.children[0]?.text}
+                    </p>
+                  </div>
+                  {/* <!-- End post content --> */}
+
+                  <div className="meta-bottom">
+                    <i className="bi bi-folder"></i>
+                    <ul className="cats">
+                      <li><a href="#">Business</a></li>
+                    </ul>
+
+                    <i className="bi bi-tags"></i>
+                    <ul className="tags">
+                      <li><a href="#">Creative</a></li>
+                      <li><a href="#">Tips</a></li>
+                      <li><a href="#">Marketing</a></li>
+                    </ul>
+                  </div>
+                  {/* <!-- End meta bottom --> */}
+
+                </article>
+                {/* <!-- End blog post --> */}
+
+                <div className="post-author d-flex align-items-center">
+                  <img src="/assets/img/blog/blog-author.jpg" className="rounded-circle flex-shrink-0" alt="" />
+                  <div>
+                    <h4>Jane Smith</h4>
+                    <div className="social-links">
+                      <a href="https://twitters.com/#"><i className="bi bi-twitter"></i></a>
+                      <a href="https://facebook.com/#"><i className="bi bi-facebook"></i></a>
+                      <a href="https://instagram.com/#"><i className="biu bi-instagram"></i></a>
+                    </div>
+                    <p>
+                      Itaque quidem optio quia voluptatibus dolorem dolor. Modi eum sed possimus accusantium. Quas repellat voluptatem officia numquam sint aspernatur voluptas. Esse et accusantium ut unde voluptas.
+                    </p>
+                  </div>
+                </div>
+                {/* <!-- End post author --> */}
+
+                <div className="comments">
+
+                  <h4 className="comments-count">8 Comments</h4>
+
+                  <div id="comment-1" className="comment">
+                    <div className="d-flex">
+                      <div className="comment-img"><img src="/assets/img/blog/comments-1.jpg" alt="" /></div>
+                      <div className='text-dark'>
+                        <h5><a href="">Georgia Reader</a> <a href="#" className="reply"><i className="bi bi-reply-fill"></i> Reply</a></h5>
+                        <time dateTime="2020-01-01">01 Jan,2022</time>
+                        <p>
+                          Et rerum totam nisi. Molestiae vel quam dolorum vel voluptatem et et. Est ad aut sapiente quis molestiae est qui cum soluta.
+                          Vero aut rerum vel. Rerum quos laboriosam placeat ex qui. Sint qui facilis et.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <!-- End comment #1 --> */}
+
+
+                  <div className="reply-form text-dark">
+
+                    <h4 >Leave a Reply</h4>
+                    <p>Your email address will not be published. Required fields are marked * </p>
+                    <form action="">
+                      <div className="row">
+                        <div className="col-md-6 form-group">
+                          <input name="name" type="text" className="form-control" placeholder="Your Name*" />
+                        </div>
+                        <div className="col-md-6 form-group">
+                          <input name="email" type="text" className="form-control" placeholder="Your Email*" />
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col form-group">
+                          <input name="website" type="text" className="form-control" placeholder="Your Website" />
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col form-group">
+                          <textarea name="comment" className="form-control" placeholder="Your Comment*"></textarea>
+                        </div>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Post Comment</button>
+
+                    </form>
+
+                  </div>
+
+                </div>
+                {/* <!-- End blog comments --> */}
+
+              </div>
+
+              <div className="col-lg-4">
+
+                <div className="sidebar">
+
+                  <div className="sidebar-item search-form">
+                    <h3 className="sidebar-title">Search</h3>
+                    <form action="" className="mt-3">
+                      <input type="text" className='bg-white' />
+                      <button type="submit"><i className="bi bi-search"></i></button>
+                    </form>
+                  </div>
+                  {/* <!-- End sidebar search formn--> */}
+
+                  <div className="sidebar-item categories">
+                    <h3 className="sidebar-title">Categories</h3>
+                    <ul className="mt-3">
+                      <li><a href="#">General Knowledge <span>(25)</span></a></li>
+                      <li><a href="#">Current Affairs <span>(12)</span></a></li>
+                    </ul>
+                  </div>
+                  {/* <!-- End sidebar categories--> */}
+
+                  <div className="sidebar-item recent-posts">
+                    <h3 className="sidebar-title">Recent Posts</h3>
+
+                    <div className="mt-3">
+
+                      <div className="post-item mt-3">
+                        <img src="/assets/img/blog/blog-recent-1.jpg" alt="" />
+                        <div>
+                          <h4><a href="blog-details.html">Nihil blanditiis at in nihil autem</a></h4>
+                          <time dateTime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+                      </div>
+                      {/* <!-- End recent post item--> */}
+
+                      <div className="post-item">
+                        <img src="/assets/img/blog/blog-recent-5.jpg" alt="" />
+                        <div>
+                          <h4><a href="blog-details.html">Et dolores corrupti quae illo quod dolor</a></h4>
+                          <time dateTime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+                      </div>
+                      {/* <!-- End recent post item--> */}
+
+                    </div>
+
+                  </div>
+                  {/* <!-- End sidebar recent posts--> */}
+
+                  <div className="sidebar-item tags">
+                    <h3 className="sidebar-title">Tags</h3>
+                    <ul className="mt-3">
+                      <li><a href="#">Economics</a></li>
+                      <li><a href="#">CUET</a></li>
+                      <li><a href="#">Business</a></li>
+                    </ul>
+                  </div>
+                  {/* <!-- End sidebar tags--> */}
+
+                </div>
+                {/* <!-- End Blog Sidebar --> */}
+
+              </div>
+            </div>
+
+          </div>
+        </section>
+        {/* <!-- End Blog Details Section --> */}
+
+
+      </main>
+      {/* <!-- End #main --> */}
+
+      <Footer />
+
+    </>
   )
 }
 
@@ -15,7 +242,7 @@ export async function getStaticPaths() {
   )
 
   return {
-    paths: paths.map((slug) => ({params: {slug}})),
+    paths: paths.map((slug) => ({ params: { slug } })),
     fallback: true,
   }
 }
