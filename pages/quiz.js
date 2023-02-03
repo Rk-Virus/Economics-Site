0625f86e27dcd484bc953b98e26ee6e9c020986c
bil4l755 <abdulbilal755755@gmail.com>
2023-02-03 20:01:51 +0530

    test commit

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-// import Sidebar from '../components/Sidebar'
 
 function quiz() {
     return (
