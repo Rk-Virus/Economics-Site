1f60b70f2aff9dae33470645b581f77e6daeb627
bil4l755 <abdulbilal755755@gmail.com>
2023-02-05 15:56:33 +0530

    ...

--- client.js
+++ client.js
@@ -5,5 +5,5 @@ export default sanityClient({
   projectId: 'iuvwtgif', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   apiVersion: "2022-03-25",
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: false // `false` if you want to ensure fresh data
 })
